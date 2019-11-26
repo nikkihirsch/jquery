28d66777bcc212a0d42b69f7cbf146ff98d0b382
nikkihirsch <46971278+nikkihirsch@users.noreply.github.com>
2019-11-26 11:54:21 -0500

    Add files via upload
    fixed the jquery for challenge 3

--- jq/challenge3.js
+++ jq/challenge3.js
@@ -12,11 +12,18 @@ $(document).ready(function(){
        else{
             alert("You must pick a fruit!");
             event.preventDefault();
-    }
+        }
+        if (standing.is(":checked")){
+            console.log("checked fruit");
+            input = true;
+        }
+        else{
+            alert("You must pick a class standing!");
+            event.preventDefault();
+        }
 
     return input;   
         
     })
 
 })
-
